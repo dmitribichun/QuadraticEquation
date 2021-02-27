f65d9c0e749b8ef46da6c043dbbc68781a1a77f0
Bichun Dmitri <79221347+dmitribichun@users.noreply.github.com>
2021-02-27 14:41:12 +0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,15 +1,16 @@
 import math
 
-a = float(input('Введите a: '))
-b = float(input('Введите b: '))
-c = float(input('Введите c: '))
+a = float(input('Enter a: '))
+b = float(input('Enter b: '))
+c = float(input('Enter c: '))
 
 D = math.sqrt(b**2-4*a*c)
 
 root1 = (-b + D)/(2*a)
 root2 = (-b - D)/(2*a)
 
-print('Решаем уравнение ax^2 + bx + c = 0 с коэффициентами a = {0} b = {1} c = {2}' .format(a,b,c))
+print('We are solving equation ax^2 + bx + c = 0') 
+print('with coeffs a = {0} b = {1} c = {2}' .format(a,b,c))
 print('({0})x^2 + ({1})x + ({2}) = 0' .format(a,b,c))
-print('Первый корень =', root1)
-print('Второй корень =', root2)
+print('The First root =', root1)
+print('The Second root =', root2)
